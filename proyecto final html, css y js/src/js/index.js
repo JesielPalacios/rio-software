407dea4f90f9cd30c14ac1a0e6804e042ffb5100
Jesiel Palacios <jopalacioss@unac.edu.co>
2021-12-04 18:23:45 -0500

    Agrega últimos cambios.

--- proyecto final html, css y js/src/js/index.js
+++ proyecto final html, css y js/src/js/index.js
@@ -17,21 +17,95 @@ function leerTexto(rutaLocal) {
   return texto;
 }
 
-function obtenerPreguntas() {
-  // console.log(leerTexto('data/preguntas.json'));
-  let preguntas = leerTexto('data/preguntas.json');
-  let preguntasParseadas = JSON.parse(preguntas);
-  pregunta = preguntasParseadas[0];
-  console.log(pregunta);
+// console.log(leerTexto('data/preguntas.json'));
+let preguntas = leerTexto('data/preguntas.json');
+let preguntasParseadas = JSON.parse(preguntas);
+let pregunta;
+let posiblesRespuestas = [];
+
+function obtenerPreguntas(numeroPregunta) {
+  pregunta = preguntasParseadas[numeroPregunta];
+  // console.log(pregunta);
   id('categoria').innerHTML = pregunta.categoria;
   id('pregunta').innerHTML = pregunta.pregunta;
-  id('boton1').innerHTML = pregunta.respuesta;
-  id('boton2').innerHTML = pregunta.incorrecta;
-  // id('boton3').innerHTML = pregunta.incorrecta2;
-  // id('boton4').innerHTML = pregunta.incorrecta3;
-  id('imagen').setAttribute('src', pregunta.imagen);
-  id('imagen').setAttribute('alt', pregunta.descripcion);
-  estilo('imagen').objectFit = pregunta.object_fit;
+  if (pregunta.categoria == 'opcion-multiple-respuesta-unica') {
+    id('boton1').innerHTML = pregunta.respuesta;
+    id('boton2').innerHTML = pregunta.incorrecta1;
+    id('boton3').innerHTML = pregunta.incorrecta2;
+    id('boton4').innerHTML = pregunta.incorrecta3;
+  } else {
+    id('boton1').innerHTML = pregunta.respuesta;
+    id('boton2').innerHTML = pregunta.incorrecta;
+    id('boton3').remove();
+    id('boton4').remove();
+  }
+
+  if (pregunta.imagen) {
+    id('imagen').setAttribute('src', pregunta.imagen);
+    estilo('imagen').height = '200px';
+    estilo('imagen').width = '100%';
+    console.log('Hay imagen');
+  } else {
+    id('imagen').remove();
+  }
+
+  // if (pregunta.imagen) {
+  //   id('imagen').setAttribute('src', pregunta.imagen);
+  //   style('imagen').height = '200px';
+  //   style('imagen').width = '100%';
+  //   id('imagen').setAttribute('alt', pregunta.descripcion);
+  //   estilo('imagen').objectFit = pregunta.object_fit;
+  // } else {
+  //   style('imagen').height = '0px';
+  //   style('imagen').width = '0px';
+  // }
 }
 
-obtenerPreguntas();
+obtenerPreguntas(0);
+
+// function escogerPreguntaAleatoria() {
+//   escogerPregunta(Math.floor(Math.random() * preguntasParseadas.length));
+//   // escogerPregunta(0);
+// }
+
+// escogerPreguntaAleatoria();
+
+function desordenarOpciones(pregunta) {
+  posiblesRespuestas = [
+    pregunta.respuesta,
+    pregunta.incorrecta1,
+    pregunta.incorrecta2,
+    pregunta.incorrecta3,
+  ];
+  posiblesRespuestas.sort(() => Math.random() - 0.5);
+
+  if (pregunta.incorrecta) {
+    id('boton1').innerHTML = pregunta.respuesta;
+    id('boton2').innerHTML = pregunta.incorrecta;
+    posiblesRespuestas = [pregunta.respuesta, pregunta.incorrecta];
+  } else {
+    posiblesRespuestas = [
+      pregunta.respuesta,
+      pregunta.incorrecta1,
+      pregunta.incorrecta2,
+      pregunta.incorrecta3,
+    ];
+    // id('boton1').innerHTML = pregunta.respuesta;
+    // id('boton2').innerHTML = pregunta.incorrecta1;
+    // id('boton3').innerHTML = pregunta.incorrecta2;
+    // id('boton4').innerHTML = pregunta.incorrecta3;
+    id('boton1').innerHTML = posiblesRespuestas[0];
+    id('boton2').innerHTML = posiblesRespuestas[1];
+    id('boton3').innerHTML = posiblesRespuestas[2];
+    id('boton4').innerHTML = posiblesRespuestas[3];
+  }
+}
+desordenarOpciones();
+
+let botones = [id('boton1'), id('boton2'), id('boton3'), id('boton4')];
+
+function eligeRespuesta(i) {
+  // console.log(posiblesRespuestas[i]);
+  // console.log(posiblesRespuestas);
+  console.log(i)
+}
